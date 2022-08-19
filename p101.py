fb4bb4f2a6ecb14c5d6a37f1dcf4516d21988c0e
YugAngrish <75120412+YugAngrish@users.noreply.github.com>
2022-08-19 07:27:25 +0530

    Add files via upload

---
+++ p101.py
@@ -0,0 +1,13 @@
+
+def dice(response):
+    import random 
+    y = response
+    while(response == y):
+      no = random.randint(1,6)
+      print(y)
+
+    n = response 
+    while(response == n):
+        print("Project ended")
+    print("Press y to cintinue and n to exit")
+
